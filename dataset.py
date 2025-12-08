bbbab4f09bb808c53e069458c9b8a81e8ffd7c97
minhhieu <=>
2025-12-08 11:38:23 +0700

    change dataset and finish linear regression

--- dataset.py
+++ dataset.py
@@ -3,42 +3,61 @@ from typing import Literal
 from collections import defaultdict
 import numpy as np
 
-
 class HousingDataset:
-    
+    def __init__(self, mode: Literal['train','test']='train'):
+        self.x = []
+        self.y = []
+        self.feature_names = {}  # dictionary to map column index -> feature name
+        self.n_features = 0
+        self.y_div_factor = 100000.0  # to scale down house prices
+        intify_buffer = defaultdict(dict) 
+        next_int = defaultdict(int)       
 
+        # -------------------------------
+        # 1. Read CSV and convert to numeric
+        # -------------------------------
+        with open(f'dataset/train.csv', 'r') as file:
+            reader = csv.reader(file)
+            header = next(reader)
+            self.feature_names = {i-1: name for i, name in enumerate(header[1:-1])}  # map index -> name
+            self.n_features = len(header) - 2  # exclude Id and SalePrice
 
-    
-    def __init__(self,mode :Literal['train','test'] = 'train', train_split: float = 0.8):
-        self.data = []
-        self.street2idx = defaultdict(lambda: len(self.street2idx))
-        self.city2idx = defaultdict(lambda: len(self.city2idx))
-        self.statezip2idx = defaultdict(lambda: len(self.statezip2idx))
-        self.country2idx = defaultdict(lambda: len(self.country2idx))
-        with open('dataset/data.csv', mode ='r')as file:
-            csvFile = csv.reader(file)
-            start_idx = 0 if mode == 'train' else int(0.8 *csvFile.line_num)
-            end_idx = int(0.8 *csvFile.line_num) if mode == 'train' else csvFile.line_num
+            x = []
+            y = []
+            for row in reader:
+                new_row = []
+                for i, item in enumerate(row):
+                    if i == 0:  # skip Id
+                        continue
+                    try:
+                        new_row.append(float(item))
+                    except ValueError:
+                        if item not in intify_buffer[i]:
+                            intify_buffer[i][item] = next_int[i]
+                            next_int[i] += 1
+                        new_row.append(intify_buffer[i][item])
+                y.append(new_row[-1]/self.y_div_factor)      # last column = SalePrice
+                x.append(new_row[:-1])     # all other columns = features
 
-            for line in csvFile:
-                date,price,bedrooms,bathrooms,sqft_living,sqft_lot,floors,waterfront,view,condition,sqft_above,sqft_basement,yr_built,yr_renovated,street,city,statezip,country = line
-                street_idx = self.street2idx[street]
-                city_idx = self.city2idx[city]
-                statezip_idx = self.statezip2idx[statezip]
-                country_idx = self.country2idx[country]
+        x = np.array(x, dtype=float)
+        y = np.array(y, dtype=float)
 
-                self.data.append([price, bedrooms, bathrooms, sqft_living, sqft_lot, floors, waterfront, view,
-                                  condition, sqft_above, sqft_basement, yr_built, yr_renovated,
-                                  street_idx, city_idx, statezip_idx, country_idx])
-        self.data.pop(0)
+        self.x_min = x.min(axis=0)
+        self.x_max = x.max(axis=0)
+        x_norm = (x - self.x_min) / (self.x_max - self.x_min + 1e-8)  # avoid divide by zero
 
+        split_idx = int(0.8 * len(x_norm))
+        if mode == 'train':
+            self.x = x_norm[:split_idx]
+            self.y = y[:split_idx]
+        else:
+            self.x = x_norm[split_idx:]
+            self.y = y[split_idx:]
 
 def main():
     dataset = HousingDataset(mode='train')
-    print(dataset.data[0])
-    print(dataset.street2idx)
-
-
+    print(len(dataset.x))
+    print(np.max(dataset.y), np.min(dataset.y))  
 
 if __name__ == "__main__":
     main()
--- linear_regression.py
+++ linear_regression.py
@@ -1,24 +1,31 @@
 from model import Model
-
+from dataset import HousingDataset
+from utils import mse
+import numpy as np
 
 class LinearRegression(Model):
     def __init__(self, n_features):
-        self.coefficients = None
+        self.coefficients = np.random.rand(n_features)
         self.n_features = n_features
 
-    def train(self, dataset):
-        pass
+    def train(self, dataset,steps=1000, lr=0.01):
+        for step in range(steps):
+            delta_w = np.dot(np.transpose(dataset.x),(np.dot(dataset.x,self.coefficients)-dataset.y))/len(dataset.x) 
+            self.coefficients -= lr*delta_w
 
     def evaluate(self, data):
-        return super().evaluate(data)
+        return np.dot(data.x, self.coefficients)
 
 
 
 
 def main():
-    pass
-
-
+    train_dataset = HousingDataset(mode='train')
+    test_dataset = HousingDataset(mode='test')
+    model = LinearRegression(n_features=train_dataset.n_features)
+    model.train(train_dataset,steps=1000,lr=0.05)
+    res = model.evaluate(test_dataset)
+    print(mse(res, test_dataset.y))
 
 if __name__ == "__main__":
     main()
